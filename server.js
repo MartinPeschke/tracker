7ccb0e5b1ba6c9c65a29120f202b13349429f5f8
Martin Peschke <martin@per-4.com>
2013-12-13 14:33:19 +0100

    timestamp test

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var azure = require('azure')
 var url = require("url")
 var port = process.env.PORT || 1337;
 
-var js_tracker = 'document.write("Welcome Fucker!");document.close();'
+var js_tracker = function(d){
+	return 'document.write("Welcome Fucker! - fetched at:'+d.getTime()+'");document.close();'
+}
 
 http.createServer(function(req, res) {
 	var args = url.parse(req.url, true);
@@ -14,7 +16,8 @@ http.createServer(function(req, res) {
 	};
 	if(args.pathname==='/t.js'){
 	  res.writeHead(200, { 'Content-Type': 'application/javascript' });
-	  res.end(js_tracker);
+	  var date = new Date();
+	  res.end(js_tracker(date));
 	} else {
 	  res.writeHead(200, { 'Content-Type': 'text/html' });
 	  res.end('<!DOCTYPE html><html><head></head><body><script src="/t.js"></script></body></html>');
