23e01fcfa0dbffc7ed47ee02e79594a4c394ffc1
Martin Peschke <martin@per-4.com>
2013-12-13 14:16:40 +0100

    tracker serve as t.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var http = require('http')
+var url = require("url");
+var azure = require('azure')
 var port = process.env.PORT || 1337;
+
+var js_tracker = 'document.write("Welcome Fucker!");document.close();';
+
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello fucker\n');
+	var args = url.parse(request.url, true);
+	request.method = request.method.toUpperCase();
+	request.args =
+	{
+		query: args.query
+	};
+	if(args.pathname==='t.js'){
+	  res.writeHead(200, { 'Content-Type': 'application/javascript' });
+	  res.end(js_tracker);
+	} else {
+	  res.writeHead(404, { 'Content-Type': 'text/plain' });
+	  res.end("Not Found");
+	}
 }).listen(port);
