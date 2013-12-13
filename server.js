c84a5a8e727224c739734b237c29c84657257e86
Martin Peschke <martin@per-4.com>
2013-12-13 16:43:04 +0100

    adding information

--- server.js
+++ server.js
@@ -6,21 +6,32 @@ var fs = require('fs')
 var port = process.env.PORT || 1337;
 
 fs.readFile('./static/tracker.js', 'utf8', function (err, tracker_file) {
+    fs.readFile('./static/b.gif', 'ascii', function (err, pixel) {
+        fs.readFile('./static/index.html', 'utf8', function (err, index_html) {
 
-    http.createServer(function(req, res) {
-        var args = url.parse(req.url, true);
-        req.method = req.method.toUpperCase();
-        req.args ={query: args.query};
+            http.createServer(function(req, res) {
 
-        if(args.pathname==='/t.js'){
-          res.writeHead(200, { 'Content-Type': 'application/javascript' });
-          res.end(tracker_file.replace('{{ time }}', new Date().getTime()));
+                var args = url.parse(req.url, true);
+                req.method = req.method.toUpperCase();
+                req.args ={query: args.query};
 
-        } else {
-          res.writeHead(200, { 'Content-Type': 'text/html' });
-          res.end('<!DOCTYPE html><html><head></head><body><script src="/t.js"></script></body></html>');
-        }
-    }).listen(port);
+                if(args.pathname==='/t.js'){
+                  res.writeHead(200, { 'Content-Type': 'application/javascript' });
+                  res.end(tracker_file.replace('{{ time }}', new Date().getTime()));
 
+                } else if(args.pathname==='/t'){
+                    res.writeHead(200, { 'Content-Type': 'image/gif' });
+                    res.end(pixel, 'binary');
+
+                    console.log(args.query);
+
+                } else {
+                  res.writeHead(200, { 'Content-Type': 'text/html' });
+                  res.end(index_html);
+                }
+
+            }).listen(port);
+        });
+    });
 });
 
--- static/tracker.js
+++ static/tracker.js
@@ -1,4 +1,39 @@
 (function(){
-document.write("Welcome home! - fetched at:{{ time }}");
-document.close();
+    var domain, siteId
+        , sl = Array.prototype.slice
+        , preQ = window[HNCWebTrckrObject].q
+        , serialize = function(obj) {
+          var str = [];
+          for(var p in obj)
+            if (obj.hasOwnProperty(p)) {
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+            }
+          return str.join("&");
+        }
+        , pixel = function(args){
+            args.siteId = siteId;
+            args.domain = domain;
+            args.url=window.location.href;
+
+            var body = document.getElementsByTagName('body')[0]
+                , a = document.createElement("img");
+            a.async=1;
+            a.style.display='none';
+            a.src="/t?"+serialize(args);
+            body.parentNode.appendChild(a);
+        }
+        , _hnc = function(cmd){
+            if(cmd==='create'){
+                siteId = arguments[1];
+                domain = arguments[2];
+                pixel({'cmd':"page_view"})
+            } else {
+                pixel({'cmd':cmd, args: sl.call(arguments, 1)})
+            }
+        };
+
+        for(var i=0;i<preQ.length;i++){
+            _hnc.apply(window, preQ[i])
+        }
+        window[HNCWebTrckrObject] = _hnc;
 }(window));
